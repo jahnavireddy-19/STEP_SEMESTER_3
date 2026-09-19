406a5465fd617f46152a25fc8f3acfc2566c1e45
jahnavireddy-19 <cc8979@srmist.edu.in>
2026-09-19 22:51:35 +0530

    add Q5 reverse customer name

---
+++ week1/Q5.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Q5 {
+
+    public static String reverseCustomerName(String customerName) {
+
+        char[] characters = customerName.toCharArray();
+        String reversedName = "";
+
+        for (int i = characters.length - 1; i >= 0; i--) {
+            reversedName = reversedName + characters[i];
+        }
+
+        return reversedName;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter customer name: ");
+        String customerName = scanner.nextLine();
+
+        String reversedName =
+                reverseCustomerName(customerName);
+
+        System.out.println("Original Name: " + customerName);
+        System.out.println("Reversed Name: " + reversedName);
+
+        scanner.close();
+    }
+}
